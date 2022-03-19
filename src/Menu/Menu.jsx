884f2ae0845f69533dc0cde8eaa6fd732cae0b0a
monolit322 <101923873+monolit322@users.noreply.github.com>
2022-03-19 12:12:58 +0300

    MARS

---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Menu.css'
+
+const Menu = ({header, items, active, setActive}) => {
+    return (
+        <div className={active ? 'menu active' : 'menu'} onClick={() => setActive(false)}>
+            <div className="blur"/>
+            <div className="menu__content" onClick={e => e.stopPropagation()}>
+                <div className="menu_header">{header}</div>
+                <ul>
+                    {items.map(item =>
+                        <li>
+                            <a href={item.href}>{item.value}</a>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Menu;
